c447f0a043b4936ce597a7f0f12cf80d48199bae
Alon Krymgand <me@alon.kr>
2025-02-23 23:09:59 +0200

    Added `BuildTool` implementation

---
+++ load/build_version/build_tool.go
@@ -0,0 +1,26 @@
+package build_version
+
+import "alon.kr/x/macho/utils"
+
+type ToolType uint32
+
+const (
+	// Source: https://github.com/apple-oss-distributions/xnu/blob/8d741a5de7ff4191bf97d57b9f54c2f6d4a15585/EXTERNAL_HEADERS/mach-o/loader.h#L1290
+
+	ToolTypeClang ToolType = 1
+	ToolTypeSwift ToolType = 2
+	ToolTypeLd    ToolType = 3
+)
+
+type BuildTool struct {
+	ToolType ToolType
+
+	// Version of the tool.
+	// Seems like each tool defines its own versioning scheme, and there is no
+	// unified way to format those 32 bits.
+	Version uint32
+}
+
+func (tool BuildTool) MarshalBinary() ([]byte, error) {
+	return utils.GenericMarshalBinary(tool)
+}
